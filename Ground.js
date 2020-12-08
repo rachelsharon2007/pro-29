a96f355b908cea605293e1c4b753f21ccce8195b
rachelsharon2007 <72387836+rachelsharon2007@users.noreply.github.com>
2020-12-08 10:46:55 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -1,17 +1,15 @@
 class Ground {
-    constructor(x,y,width,height) {
+    constructor() {
       var options = {
           isStatic: true
       }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
+      this.body = Bodies.rectangle(450,390,900,20,options);
       World.add(world, this.body);
     }
     display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill("Black");
-      rect(pos.x, pos.y, this.width, this.height);
+        noStroke();
+        fill(188,67,67);
+        rectMode(CENTER);
+        rect(this.ground.position.x,this.ground.position.y,900,20);
     }
-  };
+  }
